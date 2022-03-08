9931a4646be420f1e2060b6914080c2583e721ca
Dhravya <dhravyashah@gmail.com>
2022-03-08 13:14:39 +0530

    added comments

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ use lettre::{Message, SmtpTransport, Transport};
 use std::env;
 
 fn main() {
+    // Loads dotenv
     dotenv::dotenv().ok();
 
+    // Collects the args as a Vector
     let args: Vec<String> = std::env::args().collect();
 
     let send_to_email = args[1].clone();
@@ -22,7 +24,12 @@ fn main() {
         return;
     }
     let message = args[2..].join(" ");
+
+    // The first 4 words will be the subject
+    // TODO: Make this better and more robust
     let subject: String = args[2..6].join(" ");
+
+    // Builds the actual email itself, loads variables from env
     let email = Message::builder()
         .from(env::var("EMAIL").unwrap().parse().unwrap())
         .to(send_to_email.parse().unwrap())
@@ -30,13 +37,16 @@ fn main() {
         .body(message)
         .unwrap();
 
+    // Sets the credentials for mailserver
     let creds = Credentials::new(env::var("EMAIL").unwrap(), env::var("PASSWORD").unwrap());
 
+    // The object that actually builds the email
     let mailer = SmtpTransport::relay("smtp.gmail.com")
         .unwrap()
         .credentials(creds)
         .build();
 
+    // Sends the email
     match mailer.send(&email) {
         Ok(_) => println!("Email sent successfully!"),
         Err(e) => panic!("Could not send email: {:?}", e),
